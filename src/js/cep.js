cb1a384c70bfcb5df8f897b5fe9a4b8a76c5c931
marvinamorim <marvinamorim@gmail.com>
2021-05-04 13:48:29 -0300

    only creating keys for items that were configured

--- src/js/cep.js
+++ src/js/cep.js
@@ -2,30 +2,29 @@ const cepAutoFill = function() {
   function get() {
     let items =  {}
     itemCEP = this.action.attribute01;
-    items.logradouro = this.action.attribute02;
-    items.complemento = this.action.attribute03;
-    items.bairro = this.action.attribute04;
-    items.localidade = this.action.attribute05;
-    items.uf = this.action.attribute06;
+    if (this.action.attribute02) items.logradouro = this.action.attribute02;
+    if (this.action.attribute03) items.complemento = this.action.attribute03;
+    if (this.action.attribute04) items.bairro = this.action.attribute04;
+    if (this.action.attribute05) items.localidade = this.action.attribute05;
+    if (this.action.attribute06) items.uf = this.action.attribute06;
     let cep = apex.item(itemCEP).getValue().replace(/\D/g, '');
     var validacep = /^[0-9]{8}$/;
     if (!cep) {
-      error("O campo CEP precisa estar preenchido.");
+      error("O campo CEP precisa estar preenchido.", itemCEP);
       fill(items, "");
       return
     }
     if (!validacep.test(cep)) {
-      error("CEP inválido, verifique o CEP e tente novamente.");
+      error("CEP inválido, verifique o CEP e tente novamente.", itemCEP);
       fill(items, "");
       return
     }
-    load.on();
+    apex.message.clearErrors();
     $.getJSON(`https://viacep.com.br/ws/${cep}/json`)
     .then(function(data){
-      load.off();
       console.log(data);
       if (data.erro) {
-        error("CEP inválido, verifique o CEP e tente novamente.");
+        error("CEP inválido, verifique o CEP e tente novamente.", itemCEP);
         data = "";
       }
       fill(items, data);
@@ -39,43 +38,15 @@ const cepAutoFill = function() {
     }
   }
 
-  //short handle for page success
-  function success(message,autoHide) {
-    apex.message.showPageSuccess(message);
-    if (!isNaN(autoHide)) setTimeout(() => apex.message.hidePageSuccess(), autoHide);
-  }
   //easier handle for apex page error
-  function error(messages,autoHide) {
+  function error(messages,pageItem) {
     apex.message.clearErrors();
-    if (!Array.isArray(messages)) {
-      apex.message.showErrors({
-        type:     "error",
-        location: "page",
-        message:  messages
-      });
-    } else {
-      var errors = [];
-      messages.map(function(message) {
-        errors.push({
-          type:     "error",
-          location: "page",
-          message:  message
-        });
-      });
-      apex.message.showErrors(errors);
-    }
-    if (!isNaN(autoHide)) setTimeout(() => apex.message.clearErrors(), autoHide);
-  }
-  //functions to add/remove loading from apex.
-  var load = {
-    on: function() {
-      if ($(".u-Processing").length > 0) console.log("Loading already on.");
-      else $wP = apex.widget.waitPopup();
-    },
-    off: function() {
-      if ($(".u-Processing").length > 0) setTimeout(function(){$wP.remove();},200);
-      else console.log("Loading not on.")
-    }
+    apex.message.showErrors({
+      type:     "error",
+      location: ["page", "inline"],
+      pageItem: pageItem,
+      message:  messages
+    });
   }
   return {
     get
